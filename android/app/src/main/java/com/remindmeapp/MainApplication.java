edd36356f160d92312aec2b211e150418ac6ca9f
Marlon Mejía <marlonme.09@hotmail.com>
2018-02-21 11:08:26 -0600

    Cambio de dependencias
    Se cambiaron algunas depencias ya que se volvio a crear el proyecto y re
    reemplazaron algunos archivos.

--- android/app/src/main/java/com/remindmeapp/MainApplication.java
+++ android/app/src/main/java/com/remindmeapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.remindmeapp;
 
 import android.app.Application;
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 
-AppRegistry.registerComponent('SteppedRemindMeApp', () => App);
+AppRegistry.registerComponent('RemindMeApp', () => App);
